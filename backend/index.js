417cf6f0e29934dc8079a0f4fc33b6f99c52d797
Webcaique <sidraocaique@gmail.com>
2026-08-13 02:07:56 -0300

    mensagem do desafio

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 
 /**
- * RESOLUÇÃO DO EXERCÍCIO 
- * Aqui vamos ter uma lista que vai ter os usuários com um id e um nome
- * {id:"", nome:""}
- * Vamos ter as 4 operações básicas do CRUD
+ * DESAFIO: pequena tela de registrar e seguir
+ * Seu objetivo é criar um pequeno servidor que vai receber a requisição de resgistrar
+ * Daí vai registrar o usuário e ele vai ter a opção de seguir a Dona Benta e, também, quem está seguindo ela
  */
 
 const express = require("express");
@@ -13,46 +12,6 @@ const app = express();
 app.use(express.json());
 
 let usuarios = [];
-
-app.get("/toda-informacao", function (req, res){
-    
-    return res.status(200).send(usuarios);
-});
-
-app.post("/resgistar-usuario", function (req, res){
-    console.log("Criando um usuário");
-    if(req.body.nome.length <= 0) return res.status(400).send("USUÁRIO VAZIO");
-
-    usuarios.push({
-        id: crypto.randomUUID(),
-        nome:req.body.nome
-    });
-    return res.status(200).send("Usuário criado!");
-});
-
-app.patch("/atualizar-usuario/:id", function (req, res){
-    /**
-     * ATUALIZA O USUÁRIO DE ACORDO COM O ID
-     */
-    console.log("Atualizando o usuário");
-    let idx = req.params.id;
-    let nome = req.body.nome;
-    let user = usuarios.find(item => item.id === idx);
-
-    if(user === undefined) return res.status(404).send("USUÁRIO NÃO ENCONTRADO!");
-    user.nome = nome;
-
-    return res.status(200).send("Usuário atualizado!");
-});
-
-app.delete("/deletar/:id", function (req, res){
-    console.log("Apagando o usuário");
-    if(!req.params.id) return res.status(400).send("ID VAZIO!");
-
-    usuarios = usuarios.filter(item => item.id != req.params.id);
-    
-    return res.status(200).send("DELETADO!");
-});
 
 app.listen(8000, function (erro){
     console.log(erro);
